606b74b2301b50ce44a7d3dc4a0204f9339478e3
takashi <takashi@MBP2024.local>
2026-01-18 10:57:14 +0900

    feat: add SPA entry controller and fallback routing

---
+++ src/Controller/SpaController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class SpaController extends AbstractController
+{
+    #[Route('/', name: 'spa_index')]
+    #[Route('/{path}', name: 'spa_fallback', requirements: ['path' => '.*'])]
+    public function index(): Response
+    {
+        $path = __DIR__ . "/../../public/dev/index.html";
+        return new Response(file_get_contents($path));
+    }
+}
