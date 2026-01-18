99137d88c906dbd663369838518a1adfd44ede51
takashi <takashi@MBP2024.local>
2026-01-18 14:57:07 +0900

    style: composer check

--- src/Controller/SpaController.php
+++ src/Controller/SpaController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,7 +14,8 @@ final class SpaController extends AbstractController
     #[Route('/{path}', name: 'spa_fallback', requirements: ['path' => '.*'])]
     public function index(): Response
     {
-        $path = __DIR__ . "/../../public/dev/index.html";
+        $path = __DIR__.'/../../public/dev/index.html';
+
         return new Response(file_get_contents($path));
     }
 }
