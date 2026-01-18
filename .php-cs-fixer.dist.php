fe9457cbf58acbb15cdfe1f33126fe57f7bcf42d
takashi <takashi@MBP2024.local>
2026-01-18 14:01:34 +0900

    chore: update php-cs-fixer rules and strict typing settings

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,13 +1,20 @@
 <?php
 
-$finder = (new PhpCsFixer\Finder())
-    ->in(__DIR__)
-    ->exclude('var')
+declare(strict_types=1);
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__.'/src')
+    // ->in(__DIR__.'/tests')
 ;
 
-return (new PhpCsFixer\Config())
+return new PhpCsFixer\Config()
+    ->setRiskyAllowed(true)
     ->setRules([
+        '@PSR12' => true,
         '@Symfony' => true,
+        'yoda_style' => false,
+        'strict_param' => true,
+        'declare_strict_types' => true,
+        'return_type_declaration' => true,
     ])
-    ->setFinder($finder)
-;
+    ->setFinder($finder);
