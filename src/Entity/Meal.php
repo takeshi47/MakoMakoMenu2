34c1ae2228c2b7671d737131a47938550b0cc2d6
takashi <takashi@MBP2024.local>
2026-02-02 21:25:37 +0900

    fix: fix unique constraint definition

--- src/Entity/Meal.php
+++ src/Entity/Meal.php
@@ -8,16 +8,13 @@ use App\Repository\MealRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\UniqueConstraint;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 #[ORM\Entity(repositoryClass: MealRepository::class)]
-#[ORM\Table(
-    uniqueConstraints: [
-        new ORM\UniqueConstraint(
-            name: 'uq_daily_meal_type',
-            columns: ['daily_id', 'meal_type']
-        ),
-    ]
+#[UniqueConstraint(
+    name: 'uq_daily_meal_type',
+    columns: ['daily_id', 'meal_type']
 )]
 class Meal
 {
