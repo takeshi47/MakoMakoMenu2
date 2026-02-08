f1e7eeab9996842a0f8f6b0256e18deb76011fca
takashi <takashi@MBP2024.local>
2026-02-08 21:17:48 +0900

    refactor: remove unused DailyDisplay interface

--- frontend/src/app/models/daily.ts
+++ frontend/src/app/models/daily.ts
@@ -6,10 +6,3 @@ export interface Daily {
   meals: Meal[] | null;
 }
 
-// 追加するインターフェース
-export interface DailyDisplay {
-  date: string; // 表示する日付
-  isRegistered: boolean; // 登録済みかどうか
-  daily?: Daily; // 登録済みの場合、Dailyエンティティ
-}
-
